35a83d196cda1f8f8e8c27a0fe97e3c63630e3e2
Marcos Minond <minond.marcos@gmail.com>
2018-11-17 14:34:30 -0700

    binding all available functions

--- lib/ffi/base32.rb
+++ lib/ffi/base32.rb
@@ -6,15 +6,18 @@ module FFI
 
     ffi_lib "vendor/libbase32.dylib"
 
-    attach_function :_zbase32_encode, :zbase32_encode, [:string, :string], :string
-    attach_function :_zbase32_decode, :zbase32_decode, [:string, :string], :string
+    def self.bind(label)
+      attach_function :"_#{label}_encode", :"#{label}_encode", [:buffer_in, :buffer_out], :string
+      attach_function :"_#{label}_decode", :"#{label}_decode", [:buffer_in, :buffer_out], :string
 
-    def self.zbase32_encode(text)
-      _zbase32_encode text, ""
+      define_singleton_method :"#{label}_encode" do |text| self.send :"_#{label}_encode", text, "" end
+      define_singleton_method :"#{label}_decode" do |text| self.send :"_#{label}_decode", text, "" end
     end
 
-    def self.zbase32_decode(text)
-      _zbase32_decode text, ""
-    end
+    bind :crockford
+    bind :nintendo
+    bind :rfc4648
+    bind :triacontakia
+    bind :zbase32
   end
 end
